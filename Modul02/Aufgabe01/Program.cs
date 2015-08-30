d498be417e598fde5fe2003ef71c1f35f965d6c2
Владимир Тодоров <wtodorov@abv.bg>
2015-08-30 17:52:44 +0300

    Модул 02. Aufgabe 01

---
+++ Modul02/Aufgabe01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aufgabe01
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string[] row = {"портокали","банани","лимони"};
+			var r = new Random();
+			int a1 = r.Next(0,3);
+			int a2 = r.Next(0,3);
+			int a3 = r.Next(0,3);
+			Console.WriteLine(row[a1]+" "+row[a2]+" "+row[a3]); 
+		}
+	}
+}
