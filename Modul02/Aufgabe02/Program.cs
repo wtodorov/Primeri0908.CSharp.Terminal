a68b7dbfddbe60e67970a3b3a3beb5010780052f
Владимир Тодоров <wtodorov@abv.bg>
2015-08-30 19:27:31 +0300

    Модул 02. Aufgabe 02

---
+++ Modul02/Aufgabe02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aufgabe02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string[,] shah = new String[3,3]; 
+			shah[1,1] = "X"; 
+			Console.WriteLine(shah[1,1]); 
+		}
+	}
+}
