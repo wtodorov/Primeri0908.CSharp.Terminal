fb08cb99fbe737dd786f2e1870c6b4389ed8eaf4
Владимир Тодоров <wtodorov@abv.bg>
2015-09-02 23:42:58 +0300

    Модул 02. Видео 09 - Работа с enum

--- Modul02/TableProject/Program.cs
+++ Modul02/TableProject/Program.cs
@@ -4,18 +4,21 @@ namespace TableProject
 {
 	class MainClass
 	{
+		enum ti{ime=0, familia, godini};
+
 		public static void Main (string[] args)
 		{
 			string[,] table = new string[2, 3];
 			//Въвеждане на първи ред
-			table[0,0] ="Владимир"; table[0,1]="Тодоров"; table[0,2]="59";
+			table[0,(int) ti.ime] ="Владимир"; table[0, (int) ti.familia]="Тодоров"; table[0,(int) ti.godini]="59";
 			//Въвеждане на втори ред
-			table[1,0]="Гинка"; table[1,1]="Тодорова"; table[1,2]="62";
+			table[1,(int) ti.ime]="Гинка"; table[1,(int) ti.familia]="Тодорова"; table[1,(int) ti.godini]="62";
 			//Печат
 			Console.WriteLine((table[0,0])[0] + "." + table[0,1] + "," + table[0,2] + "г.");
 			Console.Write ("Кой искате да видите: ");
 			int _index = Convert.ToInt32(Console.ReadLine ()) - 1;
-			Console.WriteLine ((table [_index, 0]) [0] + "." + table [_index, 1] + ", " + table [_index, 2] + " г.");
+			Console.WriteLine ((table [_index, (int) ti.ime]) [0] + "." + table [_index, (int) ti.familia] + ", " + table [_index, (int) ti.godini] + " г.");
+
 //			string[] row = new string[3];
 //			row [0] = "Ред 1, ";
 //			row [1] = "Ред 2, ";
