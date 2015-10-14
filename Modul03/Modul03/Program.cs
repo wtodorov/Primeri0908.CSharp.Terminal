988817645522238acc32b6a23ed12d06502aed89
Владимир Тодоров <wtodorov@abv.bg>
2015-10-14 22:59:59 +0300

    Модул 03, Видео 03 - математически операции

--- Modul03/Modul03/Program.cs
+++ Modul03/Modul03/Program.cs
@@ -6,14 +6,23 @@ namespace Modul03
 	{
 		public static void Main (string[] args)
 		{
-			double a = 0, b = 0;
+			double a = 0, b = 0, c = 0;
 			Console.Write ("> a ="); a = Convert.ToDouble (Console.ReadLine ());
 			Console.Write ("> b ="); b = Convert.ToDouble (Console.ReadLine ());
-
+			// Форматиране с дименсия
 			string _format = "#0.00 m3";
-			Console.WriteLine("\n\n> a/b =" + (a/b).ToString(_format));
 
+			// Степенуване
+			c = Math.Pow ( a , b );
+			Console.WriteLine("\n\n> a ^ b =" + c.ToString(_format));
 
+			// Коренуване
+			c = Math.Sqrt ( b );
+			Console.WriteLine("\n\n> b ^ 1/2 =" + c.ToString(_format));
+
+			// Закръгление
+			c = Math.Round ( a / b );
+			Console.WriteLine("\n\n> (a / b) =" + c.ToString());
 		}
 	}
 }
