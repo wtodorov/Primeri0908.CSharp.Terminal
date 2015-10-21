f052989e2e2b479c512dc5717c4d34d9c44a9c8b
Владимир Тодоров <wtodorov@abv.bg>
2015-10-21 23:41:04 +0300

    Модул 03, Видео 06, Работа с условни оператори if

--- Modul03/BollExplore/Program.cs
+++ Modul03/BollExplore/Program.cs
@@ -9,24 +9,34 @@ namespace BollExplore
 			// bool _b = true;
 			// bool _test = 5 > 10;
 			// Console.WriteLine (_test);
-			int[] _i = new int[]{4,3,2};
+     		int[] _i = new int[]{4,3,2};
 			int _ui = Convert.ToInt32(Console.ReadLine());
-			bool _check01 = _ui <= _i.Length;
-			bool _check02 = _ui > 0;
 
-			bool _exp01 = _check01 && _check02; // логическо и "&&"
-			bool _exp02 = _check01 || _check02; // логическо или "||"
-			bool _exp03 = ! _check01;           // отрицание "!"
+//			bool _check01 = _ui <= _i.Length;
+//			bool _check02 = _ui > 0;
+//
+//			bool _exp01 = _check01 && _check02; // логическо и "&&"
+//			bool _exp02 = _check01 || _check02; // логическо или "||"
+//			bool _exp03 = ! _check01;           // отрицание "!"
+//
+//
+//			Console.WriteLine (_check01);
+//			Console.WriteLine (_check02);
+//			Console.WriteLine ("Отрицание на" + _check01.ToString() + ":" + _exp03);
 
-
-			Console.WriteLine (_check01);
-			Console.WriteLine (_check02);
-			Console.WriteLine ("Отрицание на" + _check01.ToString() + ":" + _exp03);
-
-			//Console.WriteLine (_i[_ui -1] );
-
-
-			//Console.WriteLine ("Hello World!");
+			if (_ui > 0 && _ui <= _i.Length)
+			{
+				Console.WriteLine (_i[_ui -1] );
+			} else 
+			{   if (_ui > _i.Length) {	
+					Console.WriteLine ("Индексът е прекалено голям \n\n\n");
+				}
+			}	
+			if (_ui < 0 )
+			{
+				Console.WriteLine("Индексът е прекалено малък \n\n\n");
+			}		
+		
 		}
 	}
 }
