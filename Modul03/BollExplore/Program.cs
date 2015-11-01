77d2272bd845f1856c4d1d3475d839a5fb5c0493
Владимир Тодоров <wtodorov@abv.bg>
2015-11-01 15:17:40 +0200

    Модул 03, Видео 09 - Използване на оператор Switch

--- Modul03/BollExplore/Program.cs
+++ Modul03/BollExplore/Program.cs
@@ -6,64 +6,65 @@ namespace BollExplore
 	{
 		public static void Main (string[] args)
 		{
-			// bool _b = true;
-			// bool _test = 5 > 10;
-			// Console.WriteLine (_test);
+
      		int[] _i = new int[] { 4 , 3 , 2 };
 			int _ui = 0; //Convert.ToInt32(Console.ReadLine());
 			string _input = "0";
 
-
-			Console.WriteLine ("Моля въведете индекс:");
+			Console.Write ("Моля въведете индекс:");
 			_input = Console.ReadLine () ;
-			bool _check01 = int.TryParse ( _input , out _ui);
-			bool _check02 = _ui <= _i.Length;
-			bool _check03 = _ui > 0;
 
-			if (_check01 && _check02 && _check03) {
-				Console.WriteLine (_i [_ui - 1]);
+			bool _check01 = int.TryParse (_input, out _ui);
+
+//			_ui = (_ui <= _i.Length) ? _ui : 3;
+//				_ui = (_ui> 0) ?_ui : 1;	
+
+			switch (_ui) {
+			case 1:
+				{
+					Console.Write ("1-ви елемент от масива: ");
+					Console.WriteLine (_i [_ui - 1]);
+					break; 
+				}
+			case 2 :
+				{
+					Console.Write ("2-ри елемент от масива: ");
+					Console.WriteLine (_i [_ui - 1]);
+					break;
+				}
+			case 3:
+				{
+					Console.Write ("3-ти елемент от масива: ");
+					Console.WriteLine (_i [_ui - 1]);
+					break;
+				}
+			default :
+				{
+					Console.Write ("В масива няма елемент със стойнос за този индекс.\nОпитайте с индекс между 1 и3.\n\n\n ");
+					break;
+				}
 			}
-
-			if (!_check01){
-				Console.WriteLine ("Индекса трябва да бъде цяло число\n\n\n");
-			}
-
-			if (!_check02) {
-				Console.WriteLine ("Индекса е прекалено голям\n\n\n"); 
-			}
-
-			if (!_check03 && _check01) {
-				Console.WriteLine ("Индекса е прекалено малък\n\n\n"); 
-			}
-
-
-//			bool _check01 = _ui <= _i.Length;
-//			bool _check02 = _ui > 0;
+//			if (_check01 && _check02 && _check03) {
+//				Console.WriteLine (_i [_ui - 1]);
+//			}
 //
-//			bool _exp01 = _check01 && _check02; // логическо и "&&" =
-//			bool _exp02 = _check01 || _check02; // логическо или "||"
-//			bool _exp03 = ! _check01;           // отрицание "!"
+//			if (!_check01){
+//				Console.WriteLine ("Индекса трябва да бъде цяло число\n\n\n");
+//			}
 //
+//			if (!_check02) {
+//				Console.WriteLine ("Индекса е прекалено голям\n\n\n"); 
+//			}
 //
-//			Console.WriteLine (_check01);
-//			Console.WriteLine (_check02);
-//			Console.WriteLine ("Отрицание на" + _check01.ToString() + ":" + _exp03);
+//			if (!_check03 && _check01) {
+//				Console.WriteLine ("Индекса е прекалено малък\n\n\n"); 
+//			}
+//
 
-//			if (_ui > 0 && _ui <= _i.Length)
-//			{
-//				Console.WriteLine (_i[_ui -1] );
-//			} else 
-//			{   if (_ui > _i.Length) {	
-//					Console.WriteLine ("Индексът е прекалено голям \n\n\n");
-//				}
-//			}	
-//			if (_ui < 0 )
-//			{
-//				Console.WriteLine("Индексът е прекалено малък \n\n\n");
-//			}		
-		
+
 		}
 	}
-}
+} 
+
 	
 
